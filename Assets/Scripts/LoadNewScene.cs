e8af4272d54ce5d8af1697c48e20f3c51119a5fb
RyaXyr <113148836+RyaXyr@users.noreply.github.com>
2022-11-20 00:53:39 -0500

    Build and Play Ready
    menu was made.

---
+++ Assets/Scripts/LoadNewScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+
+    public void LoadScene_0 ()
+    {
+        SceneManager.LoadScene("Scene_0");
+    }
+
+    public void LoadScene_1 ()
+    {
+        SceneManager.LoadScene("Scene_1");
+    }
+
+
+    
+
+}
